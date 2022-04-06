c27b80ad5add002d8235670f8f2a6a94150b7263
michaelballos <mdb3091207@gmail.com>
2022-04-06 02:04:53 -0500

    Create
    scene

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Scene from './components/Scene.js'
 
 function App() {
   return (
     <div className="App">
+      <Scene />
     </div>
   );
 }
---
+++ src/components/Scene.js
@@ -0,0 +1,10 @@
+import { Canvas } from '@react-three/fiber'
+
+const Scene = () => {
+  return (
+    <Canvas>
+    </Canvas>
+  )
+}
+
+export default Scene
