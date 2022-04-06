8e908a7de6e16620e04a44a80a7d8819fda03cae
michaelballos <mdb3091207@gmail.com>
2022-04-06 05:40:31 -0500

    Center
    Text and keep 3d model fixed while text is scrollable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { useRef, useLayoutEffect } from 'react'
+import { useRef, useLayoutEffect } from 'react';
 import './App.css';
-import Scene from './components/Scene.js'
-import Text from './components/Text.js'
+import Scene from './components/Scene.js';
+import Text from './components/Text.js';
 
 function App() {
-
   return (
     <>
       <Scene />
@@ -18,54 +17,52 @@ export default App;
 /**
  *  checks if DOM is ready to prevent bugs and support SSR
  */
-const isBrowser = typeof window !== 'undefined'
+const isBrowser = typeof window !== 'undefined';
 
 /**
- *  gets the current scroll position 
+ *  gets the current scroll position
  */
 const getScrollPosition = ({ element, useWindow }) => {
-
-  // check if running in browser else return {x: 0, y: 0} default values 
-  if (!isBrowser) return { x: 0, y: 0 }
+  // check if running in browser else return {x: 0, y: 0} default values
+  if (!isBrowser) return { x: 0, y: 0 };
 
   /**
    * checks if user requested scroll position from window or element
    */
-  const target = element ? element.current : document.body
+  const target = element ? element.current : document.body;
 
-
-  const position = target.getBoundingClientRect()
+  const position = target.getBoundingClientRect();
 
   return useWindow
     ? { x: window.scrollX, y: window.scrollY }
-    : { x: position.left, y: position.top }
-}
+    : { x: position.left, y: position.top };
+};
 
 const useScrollPosition = (effect, deps, element, useWindow, wait) => {
-  const position = useRef(getScrollPosition({ useWindow }))
+  const position = useRef(getScrollPosition({ useWindow }));
 
-  let throttleTimeout = null
+  let throttleTimeout = null;
 
   const callBack = () => {
-    const currPos = getScrollPosition({ element, useWindow })
-    effect({ prevPos: position.current, currPos })
-    position.current = currPos
-    throttleTimeout = null
-  }
+    const currPos = getScrollPosition({ element, useWindow });
+    effect({ prevPos: position.current, currPos });
+    position.current = currPos;
+    throttleTimeout = null;
+  };
 
   useLayoutEffect(() => {
     const handleScroll = () => {
       if (wait) {
         if (throttleTimeout === null) {
-          throttleTimeout = setTimeout(callBack, wait)
+          throttleTimeout = setTimeout(callBack, wait);
         }
       } else {
-        callBack()
+        callBack();
       }
-    }
+    };
 
-    window.addEventListener('scroll', handleScroll)
+    window.addEventListener('scroll', handleScroll);
 
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, deps)
-}
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, deps);
+};
--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,60 +1,49 @@
-import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls, useHelper, Stars } from '@react-three/drei'
-import { useRef } from 'react'
-import { PointLightHelper } from 'three'
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls, useHelper, Stars } from '@react-three/drei';
+import { useRef } from 'react';
+import { PointLightHelper } from 'three';
 
 const Capsule = () => {
-  const mesh = useRef()
+  const mesh = useRef();
   useFrame(() => {
-    mesh.current.rotation.x = mesh.current.rotation.z += 0.01
-    mesh.current.rotation.y += 0.005
-  })
+    mesh.current.rotation.x = mesh.current.rotation.z += 0.01;
+    mesh.current.rotation.y += 0.005;
+  });
 
   return (
     <mesh ref={mesh}>
       <capsuleGeometry attach="geometry" args={[1, 3, 30]} />
-      <meshLambertMaterial wireframe attach="material" color='white' />
+      <meshLambertMaterial wireframe attach="material" color="white" />
     </mesh>
-  )
-}
+  );
+};
 
 const Lighting = () => {
-  const light = useRef()
-  useHelper(light, PointLightHelper)
+  const light = useRef();
+  useHelper(light, PointLightHelper);
 
   return (
     <>
-      <ambientLight
-        intensity={0.01}
-      />
-      <pointLight
-        ref={light}
-        position={[0, -25, 10]}
-      />
+      <ambientLight intensity={0.01} />
+      <pointLight ref={light} position={[0, -25, 10]} />
     </>
-  )
-}
-
+  );
+};
 
 const Scene = () => {
-
   return (
-    <div className='sceneContainer'>
-      <Canvas
-        camera={{
-          position: [0, 5, 5],
-          fov: 75,
-        }}
-      >
-        <gridHelper
-          args={[20, 20, 'white', 'red']}
-        />
-        <Stars />
-        <Lighting />
-        <Capsule />
-      </Canvas>
-    </div>
-  )
-}
+    <Canvas
+      camera={{
+        position: [0, 5, 5],
+        fov: 75,
+      }}
+    >
+      <gridHelper args={[20, 20, 'white', 'red']} />
+      <Stars />
+      <Lighting />
+      <Capsule />
+    </Canvas>
+  );
+};
 
-export default Scene
+export default Scene;
