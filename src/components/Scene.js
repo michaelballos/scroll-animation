d98d1bd8b9de0d26955cad3af3c96cac5b99a646
michaelballos <mdb3091207@gmail.com>
2022-04-06 02:26:41 -0500

    Add
    Capsule

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,8 +1,29 @@
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { useRef } from 'react'
+
+const Shape = () => {
+  const mesh = useRef()
+
+  useFrame(() => {
+    mesh.current.rotation.x = mesh.current.rotation.z += 0.01
+    mesh.current.rotation.y += 0.005
+  })
+
+
+  return (
+    <mesh ref={mesh}>
+      <capsuleGeometry attach="geometry" args={[1, 1, 32]} />
+      <meshLambertMaterial wireframe attach="material" color="red" />
+    </mesh>
+  )
+}
 
 const Scene = () => {
   return (
     <Canvas>
+      <OrbitControls />
+      <Shape />
     </Canvas>
   )
 }
